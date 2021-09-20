9224c139da9bbb97e61222812d4cffab71250ada
Mduduzi Ndhlovu <mrndhlovuie@gmail.com>
2021-09-21 00:11:01 +0100

    Updates

--- src/events/user/new-activity.ts
+++ src/events/user/new-activity.ts
@@ -1,10 +1,10 @@
-import { ACTIVITY_TYPES, Subjects } from "../../types"
+// import { ACTIVITY_TYPES, Subjects } from "../../types"
 
-export interface INewActivityEvent {
-  subject: Subjects.NewActivity
-  data: {
-    id: string
-    userId: string
-    type: ACTIVITY_TYPES
-  }
-}
+// export interface INewActivityEvent {
+//   subject: Subjects.NewActivity
+//   data: {
+//     id: string
+//     userId: string
+//     type: ACTIVITY_TYPES
+//   }
+// }
