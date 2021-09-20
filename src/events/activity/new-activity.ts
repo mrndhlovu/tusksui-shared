6641001801f6501bf0a13e2e85d7796d108116f0
Mduduzi Ndhlovu <mrndhlovuie@gmail.com>
2021-09-20 23:05:36 +0100

    Updates

--- src/events/activity/new-activity.ts
+++ src/events/activity/new-activity.ts
@@ -1,5 +1,6 @@
-import { Publisher, Subjects } from "../.."
 import { INewActivityEvent } from "../user/new-activity"
+import { Publisher } from "../../services/base-publisher"
+import { Subjects } from "../../types"
 
 export class NewActivityPublisher extends Publisher<INewActivityEvent> {
   subject: Subjects.NewActivity = Subjects.NewActivity
